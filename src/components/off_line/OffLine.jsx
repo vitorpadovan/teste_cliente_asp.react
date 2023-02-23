06abd5503a64c71bac334454d27f9343ff56c7ec
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2023-02-23 14:23:19 -0300

    Adicionando background no sistema offline

--- src/components/off_line/OffLine.jsx
+++ src/components/off_line/OffLine.jsx
@@ -1,5 +1,5 @@
 function OffLine() {
-    return <h1>Sistema offline</h1>
+    return <h1 className="bg-danger p-5 m-5 border border border-dark rounded text-center">Sistema offline</h1>
 }
 
 export default OffLine;
